95d990eff5d6cf9b1399e0ac6dc84f2cbfef6612
Pyae Min Khant <pyaeminkhant@acedatasystems.com>
2024-02-15 01:16:21 +0630

    Dropped try catch block

--- src/main/java/com/spring/rest/service/PersonService.java
+++ src/main/java/com/spring/rest/service/PersonService.java
@@ -3,10 +3,10 @@ package com.spring.rest.service;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-
 import com.spring.rest.datamodel.Person;
 import com.spring.rest.dtomodel.ApiResponse;
 import com.spring.rest.dtomodel.PersonDto;
+import com.spring.rest.exception.DataNotFoundException;
 import com.spring.rest.mapper.PersonMapper;
 import com.spring.rest.repo.PersonRepository;
 
@@ -14,27 +14,50 @@ import lombok.AllArgsConstructor;
 
 @AllArgsConstructor
 @Service
+
 public class PersonService {
-	
+
 	private PersonRepository personRepo;
-	
+
 	private PersonMapper personMapper;
 
-	public ResponseEntity<Object> savePerson(PersonDto dto){
-		try {
-			Person person = personMapper.toPerson(dto);
-			
-			Person savedPerson = personRepo.save(person);
-			
-			return ResponseEntity
-					.status(HttpStatus.CREATED)
-					.body(new ApiResponse("Person saved successfully", savedPerson));
-		}
-		catch(Exception e){
-			return ResponseEntity
-					.status(HttpStatus.INTERNAL_SERVER_ERROR)
-					.body(new ApiResponse("Failed to save person : " + e.getMessage(), null));
-		}
+	public ResponseEntity<Object> savePerson(PersonDto dto) {
+
+		Person person = personMapper.toPerson(dto);
+
+		Person savedPerson = personRepo.save(person);
+
+		return ResponseEntity.status(HttpStatus.CREATED)
+				.body(new ApiResponse("Person saved successfully", savedPerson));
+
+	}
+
+	public ResponseEntity<Object> findPersonById(PersonDto dto) {
+
+		Person person = personMapper.toPerson(dto);
+
+		Person searchedPerson = findPersonById(person);
+
+		return ResponseEntity.status(HttpStatus.OK).body(new ApiResponse("Data found", searchedPerson));
+
+	}
+
+	public Person findPersonById(Person person) {
+
+		return personRepo.findById(person.getId())
+				.orElseThrow(() -> new DataNotFoundException());
+	}
+
+	public ResponseEntity<Object> updatePerson(PersonDto dto) {
+
+		Person person = personMapper.toPerson(dto);
+
+		findPersonById(person);
+
+		Person updatedPerson = personRepo.save(person);
+
+		return ResponseEntity.status(HttpStatus.NO_CONTENT)
+				.body(new ApiResponse("Person updated successfully", updatedPerson));
+
 	}
 }
- 
