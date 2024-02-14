9eaf8e21ef1a1879ad700d24bbb8a7e73d55e5a2
Pyae Min Khant <pyaeminkhant@acedatasystems.com>
2024-02-15 01:17:15 +0630

    Added person update

--- src/main/java/com/spring/rest/controller/PersonController.java
+++ src/main/java/com/spring/rest/controller/PersonController.java
@@ -3,6 +3,7 @@ package com.spring.rest.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,22 +11,27 @@ import org.springframework.web.bind.annotation.RestController;
 import com.spring.rest.dtomodel.PersonDto;
 import com.spring.rest.service.PersonService;
 
-import lombok.extern.slf4j.Slf4j;
+import jakarta.validation.Valid;
 
 @RestController
-@Slf4j
-@RequestMapping("/api")
+@RequestMapping("/api/people")
 public class PersonController {
 	
 	@Autowired
 	private PersonService personService;
 	
-	@PostMapping("/people")
-	public ResponseEntity<Object> createPerson(@RequestBody PersonDto person){
-		
-		log.info("Request --> "+person);
+	@PostMapping
+	public ResponseEntity<Object> createPerson(@Valid @RequestBody PersonDto person){
 		
 		ResponseEntity<Object> response = personService.savePerson(person);
+		
+		return response;
+	}
+	
+	@PutMapping
+	public ResponseEntity<Object> updatePerson(@Valid @RequestBody PersonDto person){
+		
+		ResponseEntity<Object> response = personService.updatePerson(person);
 		
 		return response;
 	}
