68b8124cc0614082851f1c4c4b77408226a075d3
Pyae Min Khant <pyaeminkhant@acedatasystems.com>
2024-02-15 01:16:51 +0630

    Not blank validation added

--- src/main/java/com/spring/rest/dtomodel/PersonDto.java
+++ src/main/java/com/spring/rest/dtomodel/PersonDto.java
@@ -1,9 +1,12 @@
 package com.spring.rest.dtomodel;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
 public class PersonDto {
 	private long id;
+	
+	@NotBlank(message = "Name must not be blank")
 	private String name;
 }
