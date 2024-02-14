1b8e3f454030dadf3d307ab02c90c818470b1ba5
Pyae Min Khant <pyaeminkhant@acedatasystems.com>
2024-02-15 01:16:36 +0630

    Exception layer added

---
+++ src/main/java/com/spring/rest/exception/DataNotFoundException.java
@@ -0,0 +1,13 @@
+package com.spring.rest.exception;
+
+public class DataNotFoundException extends RuntimeException{
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1320465221802598081L;
+
+	public 	DataNotFoundException() {
+		super();
+	}
+}
---
+++ src/main/java/com/spring/rest/exception/GlobalExceptionHandler.java
@@ -0,0 +1,29 @@
+package com.spring.rest.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import com.spring.rest.dtomodel.ErrorResponse;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+	
+	@ExceptionHandler(DataNotFoundException.class)
+	public ResponseEntity<Object> handleUnexpectedException(DataNotFoundException ex){
+	
+		return ResponseEntity
+				.status(HttpStatus.NOT_FOUND)
+				.body(new ErrorResponse("Requested data not found"));
+	}
+	
+	@ExceptionHandler(RuntimeException.class)
+	public ResponseEntity<Object> handleUnexpectedException(RuntimeException ex){
+		
+		// test log here
+		
+		return ResponseEntity
+				.status(HttpStatus.INTERNAL_SERVER_ERROR)
+				.body(new ErrorResponse("An unexpected error occured"));
+	}
+}
